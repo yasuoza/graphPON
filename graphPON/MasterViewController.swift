68861dcc164e3319e09ed89f314e3f19772a2174
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-11-11 15:31:29 +0900

    Remove editButton

--- graphPON/MasterViewController.swift
+++ graphPON/MasterViewController.swift
@@ -15,7 +15,6 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
