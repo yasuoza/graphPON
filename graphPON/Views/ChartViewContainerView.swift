f0a309072898b2fdb7f738a8a9fe6daba3ea948b
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-11-15 12:03:12 +0900

    graphPON/Views/ChartViewContainerView.swift

---
+++ graphPON/Views/ChartViewContainerView.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ChartViewContainerView: UIView {
+
+    @IBOutlet weak var chartView: JBChartView!
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.chartView.reloadData()
+    }
+
+}
