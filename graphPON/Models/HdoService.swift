85148c0b68037be897f97ab0e566e9f0197c8978
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-01-12 22:29:18 +0900

    Add number property to HdoService

--- graphPON/Models/HdoService.swift
+++ graphPON/Models/HdoService.swift
@@ -6,30 +6,35 @@ class HdoService: NSObject {
         case InThisMonth, InLast30Days
     }
 
-    private(set) var hdoServiceCode: String  = ""
+    private(set) var hdoServiceCode = ""
+    private(set) var number = ""
     private(set) var packetLogs: [PacketLog] = []
     var duration: Duration = .InThisMonth {
         didSet {
-            switch self.duration {
-            case .InThisMonth:
-                self.packetLogs = self.packetLogsInThisMonth
-            case .InLast30Days:
-                self.packetLogs = self.allPacketLogs
-            }
+            self.setPacketLogsDependsOnDuration()
         }
     }
-
-    private var allPacketLogs: [PacketLog] = []
-    private lazy var packetLogsInThisMonth: [PacketLog] = { [unowned self] in
+    var allPacketLogs: [PacketLog] = [] {
+        didSet {
+            self.setPacketLogsDependsOnDuration()
+        }
+    }
+    private lazy var packetLogsInThisMonth: () -> [PacketLog] = { [unowned self] in
         return self.collectInThisMonthPacketLogs()
-    }()
+    }
 
     init(hdoServiceCode: String, packetLogs: [PacketLog]) {
         super.init()
 
         self.hdoServiceCode = hdoServiceCode
         self.allPacketLogs = packetLogs
-        self.packetLogs = packetLogsInThisMonth
+        self.packetLogs = self.packetLogsInThisMonth()
+    }
+
+    init(hdoServiceCode: String, number: String) {
+        super.init()
+        self.hdoServiceCode = hdoServiceCode
+        self.number = number
     }
 
     // MARK: - Private
@@ -48,6 +53,15 @@ class HdoService: NSObject {
                 return false
             }
             return true
+        }
+    }
+
+    private func setPacketLogsDependsOnDuration() {
+        switch self.duration {
+        case .InThisMonth:
+            self.packetLogs = self.packetLogsInThisMonth()
+        case .InLast30Days:
+            self.packetLogs = self.allPacketLogs
         }
     }
 
