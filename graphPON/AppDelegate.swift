bb6946909dd892740e3ece35305c671ed0870ab2
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-03-01 15:25:34 +0900

    Update for xcode 6.3 beta 2

--- graphPON/AppDelegate.swift
+++ graphPON/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 
-        if let tabBarController = window?.rootViewController as! UITabBarController? {
+        if let tabBarController = window?.rootViewController as? UITabBarController {
             let selectedIndex = tabBarController.selectedIndex
             NSUserDefaults.standardUserDefaults().setInteger(selectedIndex, forKey: "selectedIndex")
             for vc in tabBarController.viewControllers! {
@@ -58,6 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                         vc.storeCurrentState()
                 }
             }
+            NSUserDefaults.standardUserDefaults().synchronize()
         }
     }
 
